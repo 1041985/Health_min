fcad7f6a35e21f1405f12bf882b396c81eb96aad
1041985 <alsgk971017@gmail.com>
2022-10-14 17:15:11 +0900

    memberBean 파일 생성
    (cherry picked from commit 57dade57195159a106255df2e4c25c8fa9c56aa0)

--- src/main/java/member/model/MemberBean.java
+++ src/main/java/member/model/MemberBean.java
@@ -1,5 +1,96 @@
 package member.model;
 
 public class MemberBean {
-
+	private int num;
+	private String id;
+	private String name;
+	private String pw;
+	private String gender;
+	private String addr;
+	private String email;
+	private String hp;
+	private int authority;
+	
+	private String addr_num;
+	private String addr_first;
+	private String addr_last;
+	
+	
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHp() {
+		return hp;
+	}
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+	public int getAuthority() {
+		return authority;
+	}
+	public void setAuthority(int authority) {
+		this.authority = authority;
+	}
+	public String getAddr_num() {
+		return addr_num;
+	}
+	public void setAddr_num(String addr_num) {
+		this.addr_num = addr_num;
+	}
+	public String getAddr_first() {
+		return addr_first;
+	}
+	public void setAddr_first(String addr_first) {
+		this.addr_first = addr_first;
+	}
+	public String getAddr_last() {
+		return addr_last;
+	}
+	public void setAddr_last(String addr_last) {
+		this.addr_last = addr_last;
+	}
+	
+	
+	
+	
 }
